b67cbb23083ceb15e696c2bbae3537b725cf6f0e
cookienc <kim76744@naver.com>
2022-12-10 15:29:59 +0900

    feat: 심야 요금 할인 클래스 추가
    -> phone과의 코드 중복으로 문제 발생

---
+++ src/main/java/practice/objects/phonebill/NightlyDiscountPhone.java
@@ -0,0 +1,40 @@
+package practice.objects.phonebill;
+
+import practice.objects.Money;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NightlyDiscountPhone {
+    private static final int LAT_NIGHT_HOUR = 22;
+
+    private Money nightlyAmount;
+    private Money regularAmount;
+    private Duration seconds;
+    private List<Call> calls = new ArrayList<>();
+
+    public NightlyDiscountPhone(final Money nightlyAmount, final Money regularAmount, final Duration seconds) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+    }
+
+    public Money calculateFee() {
+        Money result = Money.ZERO;
+
+        for (Call call : calls) {
+            if (call.getFrom().getHour() >= LAT_NIGHT_HOUR) {
+                result = result.plus(
+                        nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds())
+                );
+            } else {
+                result = result.plus(
+                        regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds())
+                );
+            }
+        }
+
+        return result;
+    }
+}
