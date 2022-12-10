824f3702c956c48ac4df0bcaae06fc492e0db742
cookienc <kim76744@naver.com>
2022-12-10 17:01:16 +0900

    refactor: Playlist에서 가수를 같이 관리를 한다면 super를 호출하지 않는 상속받은 클래스에서도 수정이 발생

--- src/main/java/practice/objects/music/PersonalPlaylist.java
+++ src/main/java/practice/objects/music/PersonalPlaylist.java
@@ -3,5 +3,6 @@ package practice.objects.music;
 public class PersonalPlaylist extends Playlist {
     public void remove(final Song song) {
         getTracks().remove(song);
+        getSingers().remove(song.getSinger());
     }
 }
--- src/main/java/practice/objects/music/Playlist.java
+++ src/main/java/practice/objects/music/Playlist.java
@@ -1,16 +1,24 @@
 package practice.objects.music;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Playlist {
-    private List<Song> trackes = new ArrayList<>();
+    private List<Song> tracks = new ArrayList<>();
+    private Map<String, String> singers = new HashMap<>();
 
     public void append(final Song song) {
-        getTracks().add(song);
+        tracks.add(song);
+        singers.put(song.getSinger(), song.getTitle());
     }
 
     public List<Song> getTracks() {
-        return trackes;
+        return tracks;
+    }
+
+    public Map<String, String> getSingers() {
+        return singers;
     }
 }
