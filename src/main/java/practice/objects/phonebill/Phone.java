fcc51ac62f63d03232dd0d124e88e5f697d93351
cookienc <kim76744@naver.com>
2022-12-10 15:55:38 +0900

    refactor: phone에다가 심야 요금 기능 합침
    -> enum으로 구별해서 낮은 응집도와 높은 결합도를 가짐

--- src/main/java/practice/objects/phonebill/Phone.java
+++ src/main/java/practice/objects/phonebill/Phone.java
@@ -6,41 +6,64 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
+import static practice.objects.phonebill.Phone.PhoneType.REGULAR;
+
 public class Phone {
+    private static final int LAT_NIGHT_HOUR = 22;
+
+    enum PhoneType {
+        REGULAR,
+        NIGHTLY
+    }
+
+    private PhoneType type;
+
     private Money amount;
+    private Money nightlyAmount;
+    private Money regularAmount;
     private Duration seconds;
     private List<Call> calls = new ArrayList<>();
-    private double taxRate;
 
-    public Phone(final Money amount, final Duration seconds, final double taxRate) {
+    public Phone(final Money amount, final Duration seconds) {
         this.amount = amount;
         this.seconds = seconds;
-        this.taxRate = taxRate;
     }
 
-    public void call(Call call) {
-        calls.add(call);
+    public Phone(final Money nightlyAmount, final Money regularAmount, final Duration seconds) {
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
     }
 
-    public Money getAmount() {
-        return amount;
-    }
-
-    public List<Call> getCalls() {
-        return calls;
-    }
-
-    public Duration getSeconds() {
-        return seconds;
+    public Phone(final Money amount, final Money nightlyAmount, final Money regularAmount, final Duration seconds, final List<Call> calls) {
+        this.amount = amount;
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+        this.calls = calls;
     }
 
     public Money calculateFee() {
         Money result = Money.ZERO;
 
         for (Call call : calls) {
-            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            if (type == REGULAR) {
+                result = result.plus(
+                        amount.times(call.getDuration().getSeconds() / seconds.getSeconds())
+                );
+            } else {
+                if (call.getFrom().getHour() >= LAT_NIGHT_HOUR) {
+                    result = result.plus(
+                            nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds())
+                    );
+                } else {
+                    result = result.plus(
+                            regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds())
+                    );
+                }
+            }
         }
 
-        return result.plus(result.times(taxRate));
+        return result;
     }
 }
