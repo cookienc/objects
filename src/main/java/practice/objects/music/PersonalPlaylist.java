8af9a9e1481c9bf2d67667269e2436b977f99341
cookienc <kim76744@naver.com>
2022-12-12 14:04:24 +0900

    refactor: PersonalPlaylist와 Playlist를 합성
    - 상속보다 변경에 유연한 코드

--- src/main/java/practice/objects/music/PersonalPlaylist.java
+++ src/main/java/practice/objects/music/PersonalPlaylist.java
@@ -1,8 +1,14 @@
 package practice.objects.music;
 
-public class PersonalPlaylist extends Playlist {
+public class PersonalPlaylist {
+    private Playlist playlist = new Playlist();
+
+    public void append(final Song song) {
+        playlist.append(song);
+    }
+
     public void remove(final Song song) {
-        getTracks().remove(song);
-        getSingers().remove(song.getSinger());
+        playlist.getTracks().remove(song);
+        playlist.getSingers().remove(song.getSinger());
     }
 }
